54dda57a3cb70ee6f5a2f7c12e08cad39927977e
MrProhor <ghjif2004@mail.ru>
2021-10-31 22:08:12 +0300

    Changing the program code. Creating a menu and part of the code with data encryption
    There are extra elements to display the work inside the program. Need to delete

--- project.java
+++ project.java
@@ -1,27 +1,59 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class project {
+
     public static String encryption() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите сообщение для зашифровки: ");
-        String text = scan.nextLine();
-        return text;
+        System.out.print("\nВведите сообщение для зашифровки: ");
+        String inputText = scan.nextLine();
+        char[] text = inputText.toCharArray();
+        System.out.print("Введите '\\rand', чтобы получить случайный ключ ИЛИ введите свой ключ для зашифровки.\nЭто может быть любая последовательность любых символов, длинной " + inputText.length() + ": ");
+        String key = scan.nextLine();
+        if (key.equals("\\rand")) {
+            char[] elementsOfKey = new char[inputText.length()];
+            int randomInt;
+            double randomDouble;
+            System.out.print("Ваш случайный ключ:");
+            for (int i = 0; i < inputText.length(); ++i) {
+                randomDouble = (Math.random());
+                randomInt = (int) (randomDouble*1000);
+                text[i] = (char) (((int) text[i]) + randomInt);
+                System.out.print( (char) (randomInt));
+                elementsOfKey[i] = (((char) (randomInt)));
+            }
+            System.out.println("\n" + Arrays.toString(elementsOfKey));
+            System.out.println(text);
+        }
+        return null;
     }
 
+    /*
     public static String decryption() {
         System.out.print("Введите имеющийся у Вас ключ: ");
         Scanner scan = new Scanner(System.in);
         String inputKey = scan.nextLine();
         return inputKey;
     }
+    Эта часть кода не дописана
+    */
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String freeInput;
-        System.out.print("Здравствуйте! \n \nВыберите режим работы: \n 1) Зашифровка сообщения (\\enc) \n 2) Дешифровка сообщения (\\dec)\nВвод ответа: ");
+        int i = 0;
+        while (i!=1) {
+        System.out.print("Здравствуйте! \n \nВыберите режим работы: \n 1) Зашифровка сообщения (\\enc) \n 2) Дешифровка сообщения (\\dec)\nВвод команды: ");
         freeInput = scan.nextLine();
-        switch (freeInput) {
-            case "\\enc": encryption();
+            switch (freeInput) {
+                case ("\\enc") -> encryption();
+                case ("\\dec") -> decryption();
+                case ("\\exit") -> {
+                    System.out.print("\nВыход...");
+                    i = 1;
+                }
+                default -> System.out.print("\nОшибка! Попробуйте ввести команду ещё раз...\n\n");
+            }
         }
     }
 }
