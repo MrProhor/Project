f63a98aa3ac77444aafaf88f0f880c026bc314cf
MrProhor <ghjif2004@mail.ru>
2021-11-22 22:51:28 +0300

    Adding changes to the encryption process. Creating a template for the decryption process. Correction of minor errors

--- project.java
+++ project.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class project {
 
-    public static String encryption() {
+    public static void encryption() {
         Scanner scan = new Scanner(System.in);
         System.out.print("\nВведите сообщение для зашифровки: ");
         String inputText = scan.nextLine();
@@ -11,22 +10,22 @@ public class project {
         int i = 0;
         inputText = new String(text);
         while (i != 1) {
-            System.out.print("Введите '\\rand', чтобы получить случайный ключ ИЛИ введите свой ключ для зашифровки.\nЭто может быть любая последовательность любых символов, длинной " + inputText.length() + ": ");
+            System.out.print("Введите '\\rand', чтобы получить случайный ключ ИЛИ введите свой ключ для зашифровки.\nЭто может быть любая последовательность латинских букв, знаков препинания и цифр, длинной " + inputText.length() + ": ");
             String key = scan.nextLine();
             switch (key) {
                 case ("\\rand"):
                     int randomInt;
                     double randomDouble;
                     System.out.print("\nВаш сгенерированный ключ: ");
-                    for (i = 0; i < inputText.length(); i = i) {
+                    for (i = 0; i < inputText.length(); ) {
                         randomDouble = (Math.random());
                         randomInt = (int) (randomDouble * 1000);
-                        if ((randomInt > 31 & randomInt < 126) | (randomInt > 1039 & randomInt < 1104)) {
+                        if ((randomInt > 31 & randomInt < 127) | (randomInt > 1039 & randomInt < 1104)) {
                             System.out.print((char) (randomInt));
                             int a = (((int) text[i]) + randomInt);
-                            if (((((int) text[i]) + randomInt) > 125) & ((((int) text[i]) + randomInt) < 1040)) {
-                                while (a > 125) {
-                                    a = 31 + a - 125;
+                            if (((((int) text[i]) + randomInt) > 126) & ((((int) text[i]) + randomInt) < 1040)) {
+                                while (a > 126) {
+                                    a = 31 + a - 126;
                                 }
                                 text[i] = (char) (a);
                                 i++;
@@ -48,23 +47,38 @@ public class project {
                     break;
                 default:
                     if (key.length() == inputText.length()) {
-                        System.out.println("\nВаш ключ: " + key);
                         char[] arrayOfKey = key.toCharArray();
-                        int a = (((int) text[i]) + ((int) arrayOfKey[i]));
-                        for (i = 0; i < inputText.length(); i++) {
-                            if ((((int) arrayOfKey[i]) > 31 & ((int) arrayOfKey[i]) < 126) | (((int) arrayOfKey[i]) > 1039 & ((int) arrayOfKey[i]) < 1104)) {
-                                if (((((int) text[i]) + ((int) arrayOfKey[i])) > 125) & ((((int) text[i]) + ((int) arrayOfKey[i])) < 1040)) {
-                                    while (a > 125) {
+                        for (i = 0; i < key.length()-1; i++) {
+                            if (arrayOfKey[i] < 32 | arrayOfKey[i] > 126) {
+                                System.out.print("\nОШИБКА! Введён недопустимый символ!\n\n");
+                                i = -1;
+                                break;
+                            }
+                        }
+                        if (i == -1) {
+                            break;
+                        }
+                        System.out.println("\nВаш ключ: " + key);
+                        i = 0;
+                        int a;
+                        for (i = 0; i < inputText.length(); ) {
+                            a = ((int) text[i]) + ((int) arrayOfKey[i]);
+                            if ((((int) arrayOfKey[i]) > 31 & ((int) arrayOfKey[i]) < 127) | (((int) arrayOfKey[i]) > 1039 & ((int) arrayOfKey[i]) < 1104)) {
+                                if ((a > 126) & (a < 1040)) {
+                                    while (a > 126) {
                                         a = 31 + a - 126;
                                     }
                                     text[i] = (char) (a);
-                                } else if ((((int) text[i]) + ((int) arrayOfKey[i])) > 1103) {
+                                    i++;
+                                } else if (a > 1103) {
                                     while (a > 1103) {
                                         a = 1039 + a - 1104;
                                     }
                                     text[i] = (char) (a);
+                                    i++;
                                 } else {
                                     text[i] = (char) (a);
+                                    i++;
                                 }
                             }
                         }
@@ -77,18 +91,47 @@ public class project {
                     }
             }
         }
-        return null;
     }
 
-    /*
-    public static String decryption() {
-        System.out.print("Введите имеющийся у Вас ключ: ");
-        Scanner scan = new Scanner(System.in);
-        String inputKey = scan.nextLine();
-        return inputKey;
+    public static void decryption() {
+        int a = 1;
+        while (a != 0) {
+            System.out.print("\nВведите имеющийся у Вас текст: ");
+            Scanner scan = new Scanner(System.in);
+            String inputText = scan.nextLine();
+            char[] text = inputText.toCharArray();
+            System.out.print("Введите имеющийся у Вас ключ: ");
+            String inputKey = scan.nextLine();
+            char[] key = inputKey.toCharArray();
+            if (inputKey.length() != inputText.length()) {
+                System.out.print("ОШИБКА! Длинна ключа не совпадает с длинной зашифрованного сообщения!\n");
+            } else {
+                System.out.print("\nИсходное сообщение: ");
+                for (a = 0; a < inputText.length(); a++) {
+                    char i;
+                    if ((text[a] - key[a] > 31) & ((text[a] > 31 & text[a] < 127) | (text[a] > 1039 & text[a] < 1104))) {
+                        System.out.print((char) (text[a] - key[a]));
+                    } else if (text[a] - key[a] < 31) {
+                        i = ((char) (text[a] - 31 + 126));
+                        if (i - key[a] < 31) {
+                            i = ((char) (i - 31 + 126 - key[a]));
+                        } else {
+                            i = (char) (i -  key[a]);
+                        }
+                        System.out.print(i);
+                        break;
+                    } else if (text[a] - key[a] < 32) {
+                        i = (char) (125 + 32 - text[a] - key[a]);
+                        System.out.print(i);
+                    } else {
+                        i = (char) (text[a] - key[a]);
+                        System.out.print(i);
+                    }
+                }
+            }
+            a = 0;
+        }
     }
-    Эта часть кода не дописана
-    */
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -100,7 +143,7 @@ public class project {
             freeInput = scan.nextLine();
             switch (freeInput) {
                 case ("\\enc") -> encryption();
-                //case ("\\dec") -> decryption();
+                case ("\\dec") -> decryption();
                 case ("\\exit") -> {
                     System.out.print("\nВыход...");
                     i = 1;
